0f7a5d933938586f10510642e2b1826807f18bcc
maya8624 <maya8624@hotmail.com>
2026-01-15 21:42:45 +1100

    create folder structure

---
+++ PayPalIntegration/Controllers/PaymentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PayPalIntegration.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
