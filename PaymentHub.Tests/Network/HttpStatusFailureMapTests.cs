0529354bf6eaaadd39eb36c33601bb096932fca0
maya8624 <maya8624@hotmail.com>
2026-01-17 11:04:35 +1100

    set up test project and add HttpStatusFailureMapTests

---
+++ PaymentHub.Tests/Network/HttpStatusFailureMapTests.cs
@@ -0,0 +1,33 @@
+﻿using PaymentHub.Network;
+using PaymentHub.Network.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PaymentHub.Tests.Network
+{
+    public class HttpStatusFailureMapTests
+    {
+        [Theory]
+        [InlineData(HttpStatusCode.BadRequest, ApiFailureReason.ValidationError)]
+        [InlineData(HttpStatusCode.Unauthorized, ApiFailureReason.Unauthorized)]
+        [InlineData(HttpStatusCode.Forbidden, ApiFailureReason.Forbidden)]
+        [InlineData(HttpStatusCode.NotFound, ApiFailureReason.NotFound)]
+        [InlineData(HttpStatusCode.TooManyRequests, ApiFailureReason.RateLimited)]
+        [InlineData(HttpStatusCode.RequestTimeout, ApiFailureReason.Timeout)]
+        [InlineData(HttpStatusCode.InternalServerError, ApiFailureReason.ServerError)]
+        [InlineData((HttpStatusCode)499, ApiFailureReason.Unknown)]
+        public void Resolve_ReturnsExpectedFailureReason(HttpStatusCode statusCode, ApiFailureReason expected)
+        {
+            // Act
+            var result = HttpStatusFailureMap.Resolve(statusCode);
+
+            // Assert
+            Assert.Equal(expected, result);            
+        }
+    }
+}
